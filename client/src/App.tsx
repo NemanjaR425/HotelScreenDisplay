6032318bd961f6d663446f0cb7587d3ebf4ed47e
NemanjaR425 <nemanja425@gmail.com>
2026-04-02 13:52:00 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+/*import { Toaster } from "@/components/ui/toaster";*/
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { LanguageProvider } from "@/contexts/LanguageContext";
 import DigitalSignage from "@/components/DigitalSignage";
@@ -37,12 +37,10 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <LanguageProvider>
-          <Toaster />
+          {/* <Toaster /> */}  {/* Correct way to comment out JSX tags */}
           <Router />
         </LanguageProvider>
       </TooltipProvider>
     </QueryClientProvider>
   );
 }
-
-export default App;
